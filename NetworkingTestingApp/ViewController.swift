093beee27f6b850cbc67fa5d880dbd9ad2121d40
Petr Tartynskikh <haiden_tenno@mail.ru>
2020-06-29 14:16:06 +0300

    Initial Commit

---
+++ NetworkingTestingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingTestingApp
+//
+//  Created by Петр Тартынских  on 29.06.2020.
+//  Copyright © 2020 Петр Тартынских . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
